5e090598ebaab3f9bb82285ea52602cb10296c15
RaspiKidd <kerry@raspikidd.com>
2021-06-28 20:52:26 +0100

    about updated

--- pages/About.vue
+++ pages/About.vue
@@ -94,7 +94,7 @@
             <div class="pt-5 pb-6 px-5">
               <div class="flex items-center justify-between">
                 <NuxtLink to="/">
-                  <img class="h-8 w-auto sm:h-10" src="/static/RaspiKidd.svg" alt="Logo">
+                  <img class="h-8 w-auto sm:h-10" src="@/static/RaspiKidd.svg" alt="Logo">
                 </NuxtLink>
                 <div class="-mr-2">
                   <button type="button" @click="isOpen = !isOpen" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
