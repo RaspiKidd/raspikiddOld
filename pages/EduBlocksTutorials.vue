18efdfb72f1d50453c9dd415d04ee16797ccbe84
Kerry Kidd <kerry@raspikidd.com>
2021-11-06 12:53:46 +0000

    Throw a ghost EduBlocks

--- pages/EduBlocksTutorials.vue
+++ pages/EduBlocksTutorials.vue
@@ -245,7 +245,7 @@
                   </div>
                 </NuxtLink>
               
-                <!-- <NuxtLink to="/ThrowGhostEB" class="block mt-2">
+                <NuxtLink to="/ThrowGhostEB" class="block mt-2">
                   <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
                     <div class="flex-shrink-0">
                       <img class="h-48 w-full object-scale-down" src="@/static/images/ThrowGhost_cover.png" alt="micro:bit ghost icon">
@@ -261,7 +261,7 @@
                       </div>
                     </div>
                   </div>
-                </NuxtLink> -->
+                </NuxtLink>
               </div>
             </div>
           </div>
