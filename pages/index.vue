7cb8a5775aaa931cc404ea43f6caddf734d4e3f7
RaspiKidd <kerry@raspikidd.com>
2022-02-23 19:46:00 +0000

    Updated spacing for mobile

--- pages/index.vue
+++ pages/index.vue
@@ -202,8 +202,8 @@
 </main>
 
 <!-- Neswletter form -->
-<div class="bg-white" style ="background-color:#4392F1;">
-      <div class="py-12 mx-auto max-w-7xl sm:px-6 lg:py-16 lg:px-8">
+<div class="bg-white">
+      <div class="px-6 py-12 mx-auto max-w-7xl sm:px-6 lg:py-16 lg:px-8">
         <div
           class="py-6 bg-teal-600 rounded-lg md:py-12 md:px-12 lg:py-16 lg:px-16 xl:flex xl:items-center"
         >
