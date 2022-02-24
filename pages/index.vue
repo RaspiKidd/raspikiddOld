4202f75c3b8de8591cf5cee5bd823d2b838ac463
RaspiKidd <kerry@raspikidd.com>
2022-02-24 19:05:11 +0000

    Updated colours

--- pages/index.vue
+++ pages/index.vue
@@ -205,7 +205,7 @@
 <div class="bg-white" style="background-color:#99DFEC">
       <div class="px-6 py-12 mx-auto max-w-7xl sm:px-6 lg:py-16 lg:px-8">
         <div
-          class="py-6 bg-teal-600 rounded-lg md:py-12 md:px-12 lg:py-16 lg:px-16 xl:flex xl:items-center"
+          class="px-6 py-6 bg-teal-600 rounded-lg md:py-12 md:px-12 lg:py-16 lg:px-16 xl:flex xl:items-center"
         >
           <div class="xl:w-0 xl:flex-1">
             <h2
--- pages/microbit.vue
+++ pages/microbit.vue
@@ -191,10 +191,10 @@
         </div>
     </main>
     <!-- Tutorial Block -->
-    <div class="relative bg-gray-50 ">
+    <div class="relative">
     <!-- <div class="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8"> -->
             <div class="absolute inset-0">
-              <div class="bg-white h-1/3 sm:h-2/3"></div>
+              <div class="h-1/3 sm:h-2/3" style="background-color:#99DFEC"></div>
             </div>
             
             <div class="relative max-w-7xl mx-auto">
