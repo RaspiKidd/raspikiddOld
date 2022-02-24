3a708f059223e6640642ec97d491646ae6943057
RaspiKidd <kerry@raspikidd.com>
2022-02-24 21:59:30 +0000

    Added Privacy policy

--- pages/MakeCodeTutorials.vue
+++ pages/MakeCodeTutorials.vue
@@ -358,8 +358,14 @@
                 </a>
               </div>
               <p class="mt-8 text-base text-center text-gray-400">
-                &copy; 2021 RaspiKidd. All rights reserved.
-              </p>
+          &copy; 2021 RaspiKidd. All rights reserved. 
+          <a 
+          class ="text-blue-500" 
+          href= "/privacy_policy.pdf"
+          target="_blank"> 
+            Privacy Policy 
+          </a>
+        </p>
             </div>
           </footer>
         </div>
