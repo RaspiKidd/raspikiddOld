722f1116fc334cda7051c5da99bf577a81b4dfe7
RaspiKidd <kerry@raspikidd.com>
2022-03-04 22:20:04 +0000

    About page updated

--- pages/About.vue
+++ pages/About.vue
@@ -170,10 +170,10 @@
       <div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-30 lg:text-center">
         <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
           <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-            <span class="block text-red-700 xl:inline"> About </span>
+            <span class="block xl:inline" style = "color:#A42530"> About </span>
           </h1>
           <h2 class=" pt-8 text-2xl tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl lg:text-3xl xl:text-4xl">
-            <span class="block xl:inline" style="color:#76B82A"> 
+            <span class="block xl:inline" style="color:#0E7452"> 
               Imagine being able to program a computer that you can pick up and put in your pocket
             </span>
           </h2>
@@ -186,7 +186,7 @@
           </p>
 
           <h2 class=" pt-8 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-5xl lg:text-4xl xl:text-5xl">
-            <span class="block xl:inline sm:text-4xl md:text-5xl lg:text-4xl xl:text-5xl" style="color:#76B82A"> 
+            <span class="block xl:inline sm:text-4xl md:text-5xl lg:text-4xl xl:text-5xl" style="color:#A42530"> 
               Who we are 
             </span>
         </h2>
