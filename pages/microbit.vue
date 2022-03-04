b66f3bda1c846a4a90c321979687d76bd64f318a
RaspiKidd <kerry@raspikidd.com>
2022-03-04 21:54:47 +0000

    Updated micro:bit page

--- pages/microbit.vue
+++ pages/microbit.vue
@@ -168,10 +168,10 @@
         <div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-30 lg:text-center">
             <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
                 <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-                  <span class="block text-red-700 xl:inline"> micro:bit </span>
+                  <span class="block xl:inline" style = "color:#A42530"> micro:bit </span>
                 </h1>
                  <h2 class=" pt-8 text-2xl tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl lg:text-3xl xl:text-4xl">
-                  <span class="block xl:inline" style="color:#76B82A"> 
+                  <span class="block xl:inline" style="color:#0E7452"> 
                     The micro:bit Educational Specialist 
                   </span>
                 </h2>
@@ -194,13 +194,13 @@
     <div class="relative">
     <!-- <div class="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8"> -->
             <div class="absolute inset-0">
-              <div class="h-1/3 sm:h-2/3" style="background-color:#C9E391"></div>
+              <div class="h-1/3 sm:h-2/3" style="background-color:#a1c4b5"></div>
             </div>
             
             <div class="relative max-w-7xl mx-auto">
               <div class="text-center">
                <h2 class=" pt-8 text-4xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-5xl lg:text-4xl xl:text-5xl">
-                  <span class="block xl:inline sm:text-4xl md:text-5xl lg:text-4xl xl:text-5xl" style="color:#76B82A"> 
+                  <span class="block xl:inline sm:text-4xl md:text-5xl lg:text-4xl xl:text-5xl" style="color:#0E7452"> 
                     Tutorials 
                   </span>
                 </h2>
