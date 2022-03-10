8688f6ea787a574bca09880378d7bca96d608cc6
Kerry Kidd <kerry@raspikidd.com>
2022-03-10 20:02:10 +0000

    Added lessons to nav bar

--- pages/index.vue
+++ pages/index.vue
@@ -160,6 +160,9 @@
                                <NuxtLink to="/HireMe" class="text-base font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 p-3">
                                 Hire Us
                                </NuxtLink>
+                               <NuxtLink to="/LessonPacks" class="text-base font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 p-3">
+                                Lessons
+                               </NuxtLink>
                             </nav>
                           </div>
                         </div>
